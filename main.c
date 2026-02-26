0bffafdbba05917493a46326153e21d6f1b47d66
Aditya <rishkat1249@gmail.com>
2026-02-26 11:06:30 +0000

    Creating a blank window

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <SDL2/SDL.h>
+
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Bouncy Ball Simulation",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        0
+    );
+
+    int running = 1;
+    SDL_Event event;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = 0;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
